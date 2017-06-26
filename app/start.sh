8cd6275530a57ff2e4da6257850cd5112319de73
Alexis Susset <alexis.susset@icloud.com>
2017-06-26 09:36:05 +0200

    Adding in SSH cupport

--- app/start.sh
+++ app/start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ -n "${SSH_PASSWD}" ]]; then
+	#Set the root password
+	echo "root:$SSH_PASSWD" | chpasswd
+	#Spawn dropbear
+	dropbear -E -F &
+fi
+
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 # Enable i2c
