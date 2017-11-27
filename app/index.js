9d5db54a1cad254cf39907cd0d9c8596d55c40cb
Alexis Susset <alexis_susset@icloud.com>
2017-11-27 11:31:47 +0100

    Main update to add new download tool and md5 check

--- app/index.js
+++ app/index.js
@@ -1,22 +1,53 @@
-#!/bin/env node
-{
-    const supervisorClient = require(__dirname + '/libs/supervisorClient/supervisorClient.js');
-    const imageDownloader = require(__dirname + '/libs/imageDownloader/imageDownloader.js');
-    const guiType = "none";
-    const gui = require(__dirname + '/libs/gui/none.js');
-    const chalk = require('chalk');
-    const Writer = require(__dirname + '/libs/writer.js');
-    const debug = require('debug')('main');
+const chalk = require('chalk');
+const Writer = require(__dirname + '/libs/writer.js');
+const debug = require('debug')('main');
 
-	var fs = require('fs'),
-		curl = require('download'),
-		data_progress = [];
+var fs = require('fs'),
+	url = require('url'),
+	https = require('https'),
+	md5File = require('md5-file'),
+	data_progress = [];
+
+function bytesToSize(bytes) {
+   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+   if (bytes == 0) return '0 Byte';
+   var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+   return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+}
+
+function image_md5(message){
+	if(message == 1){
+		console.log('Starting OS image MD5 checksum');
+		// Check downloaded image MD5 against configuration
+		md5File('/data/resin.img.gz', (err, hash) => {
+			if(err) throw err
+			if(hash == process.env.ETCHER_IMAGE_MD5){
+				console.log('MD5 checksum verified successfully, start SD Writer software');
+				// Download completed and validted, starting SD Writer software
+				start_sd_writer(1);
+		  }else{
+			  console.log('Invalid MD5 checksum, re-downloading image');
+			  download(process.env.ETCHER_IMAGE_URL, '/data/resin.img.gz', image_md5);
+		  }
+		});
+	}else if(message){
+		console.log('Download failed with error: '+ message +", restarting");
+		download(process.env.ETCHER_IMAGE_URL, '/data/resin.img.gz', image_md5);
+	}
+}
+
+function start_sd_writer(){
+	// Download completed and validted, starting SD Writer software
+	console.log('SD Wirter');
+}
 
 function download(fileUrl, apiPath, callback) {
-    var url = require('url'),
-        http = require('http'),
-        p = url.parse(fileUrl),           
-        timeout = 10000; 
+	"use strict";
+	console.log('Starting OS image download');
+    var p = url.parse(fileUrl),           
+        timeout = 10000,
+        current_download_progress,
+        previous_download_progress;
     
     var file = fs.createWriteStream(apiPath);
     
@@ -27,19 +58,19 @@ function download(fileUrl, apiPath, callback) {
             callback("File transfer timeout!");
         };
     };
-    
- 
-    console.log('before');
 
-    var request = http.get(fileUrl).on('response', function(res) { 
-        console.log('in cb');           
+    var request = https.get(fileUrl).on('response', function(res) {          
         var len = parseInt(res.headers['content-length'], 10);
         var downloaded = 0;
         
         res.on('data', function(chunk) {
             file.write(chunk);
             downloaded += chunk.length;
-            console.log("Downloading " + (100.0 * downloaded / len).toFixed(2) + "% " + downloaded + " bytes" + isWin ? "\033[0G": "\r");
+            current_download_progress = (100.0 * downloaded / len).toFixed(0);
+            if(current_download_progress > previous_download_progress){
+				console.log("Downloaded " + current_download_progress + "% " + bytesToSize(downloaded));
+            }
+            previous_download_progress = current_download_progress;
             // reset timeout
             clearTimeout( timeoutId );
             timeoutId = setTimeout( fn, timeout );
@@ -47,11 +78,11 @@ function download(fileUrl, apiPath, callback) {
             // clear timeout
             clearTimeout( timeoutId );
             file.end();
-            console.log(file_name + ' downloaded to: ' + apiPath);
-            callback(null);
+            console.log('Image successfully downloaded to: ' + apiPath);
+            callback(1);
         }).on('error', function (err) {
             // clear timeout
-            clearTimeout( timeoutId );                
+            clearTimeout(timeoutId);
             callback(err.message);
         });           
     });
@@ -63,79 +94,14 @@ function download(fileUrl, apiPath, callback) {
     var timeoutId = setTimeout( fn, timeout );
 }
 
-
-	function start_sd_writer() {
-			"use strict";
-			const writer = Writer.start('/data/resin.img');
-			writer.on('progress', (data) => {
-			    debug(data);
-			    progress(data);
-			});
-			
-			writer.on('done', (data) => {
-			    debug(data);
-			    complete(data);
-			});
-			
-			writer.on('error', (error) => {
-			    console.error('Error!');
-			    console.error(error);
-			});
-		}
-		
-		let progress = function(data) {
-		    "use strict";
-		    if (isWriting(data.state.type)) {
-				if(parseInt(data.state.percentage, 0) !== data_progress[data.device]) console.log('Writing to '+data.device+' is '+ parseInt(data.state.percentage, 0) +'% complete');
-				data_progress[data.device] = parseInt(data.state.percentage, 0);
-		        gui.write(identifyDevice(data.device),data.state.percentage);
-		    } else {
-		        gui.verify(identifyDevice(data.device),data.state.percentage);
-		    }
-		};
-		
-		let complete = function(data) {
-		    "use strict";
-		    console.log('Completed write and check of image on: '+data.device);
-		    gui.done(identifyDevice(data.device));
-		};
-		
-		let identifyDevice = function(data) {
-		    "use strict";
-		    switch (data) {
-		        case "/dev/sda":
-		            return 4;
-		        case "/dev/sdb":
-		            return 3;
-		        case "/dev/sdc":
-		            return 2;
-		        case "/dev/sdd":
-		            return 1;
-		        default:
-		            return 5;
-		
-		    }
-		};
-		
-		let isWriting = function(data) {
-		    "use strict";
-		    if (data === "write") {
-		        return true;
-		    } else {
-		        return false;
-		    }
-		};
+if(process.env.ETCHER_IMAGE_URL && process.env.ETCHER_IMAGE_MD5){
+	// Check if image has already been downloaded and check it against checksum
+	if (!fs.existsSync('/data/resin.img.gz') || (fs.existsSync('/data/resin.img.gz') && process.env.ETCHER_IMAGE_OVERWRITE)){
+		download(process.env.ETCHER_IMAGE_URL, '/data/resin.img.gz', image_md5);
+	}else{
+		image_md5(1);
 	}
-
-    gui.ready();
-    
-    if(process.env.ETCHER_IMAGE_URL){
-		// Check if image has already been downloaded and check it against checksum
-		if (!fs.existsSync('/data/resin.img') || (fs.existsSync('/data/resin.img') && process.env.ETCHER_IMAGE_OVERWRITE)){
-			download(process.env.ETCHER_IMAGE_OVERWRITE, '/data/resin.img', start_sd_writer);
-		}else{
-			start_sd_writer();
-		}
-    }else{
-	    console.log('[ERROR] Please set ETCHER_IMAGE_URL to use this application');
-    }
+}else{
+    if(!process.env.ETCHER_IMAGE_URL) console.log('[ERROR] Please set ETCHER_IMAGE_URL environment variable to use this application');
+    if(!process.env.ETCHER_IMAGE_MD5) console.log('[ERROR] Please set ETCHER_IMAGE_MD5 environment variable to use this application');
+}
